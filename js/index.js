c840047050fa0f10d577b9a2ad617c917ae70d77
Aldairfelix <103163200+Aldairfelix@users.noreply.github.com>
2022-04-06 22:53:16 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,72 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.titulo', {
+        duration: 4000, 
+        origin: 'bottom', 
+        distance: '-100px'
+    });
+
+    sr.reveal('.tarta', {
+        duration: 4000, 
+        origin: 'right', 
+        distance: '-100px'
+    });
+
+    sr.reveal('.reloj', {
+        duration: 4000, 
+        origin: 'left', 
+        distance: '-100px'
+    });
+
+    sr.reveal('.horario', {
+        duration: 4000, 
+        origin: 'left', 
+        distance: '-100px'
+    });
+
+
+    sr.reveal('.nav', {
+        duration: 4000, 
+        origin: 'top', 
+        distance: '-100px'
+    });
+
+    sr.reveal('.sobre-nosotros', {
+        duration: 4000, 
+        origin: 'left', 
+        distance: '-100px'
+    });
+
+    sr.reveal('.historia', {
+        duration: 4000, 
+        origin: 'left', 
+        distance: '-100px'
+    });
+
+    sr.reveal('.variedad', {
+        duration: 4000, 
+        origin: 'bottom', 
+        distance: '-100px'
+    });
+
+    sr.reveal('.body_page', {
+        duration: 4000, 
+        origin: 'top', 
+        distance: '-100px'
+    });
+
+    sr.reveal('.container-footer-all', {
+        duration: 4000, 
+        origin: 'bottom', 
+        distance: '-100px'
+    });
+
+    sr.reveal('.scooter', {
+        duration: 5000, 
+        origin: 'right', 
+        distance: '-300px'
+    });
+
+
+    
+    
